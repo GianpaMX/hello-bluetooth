8bf7e6db562f215e59f4f8b72ad856d7cfbf80bc
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-01-26 13:57:28 -0600

    BT Status

--- app/src/main/java/io/github/gianpamx/hellobluetooth/discovery/DiscoveryActivity.kt
+++ app/src/main/java/io/github/gianpamx/hellobluetooth/discovery/DiscoveryActivity.kt
@@ -1,13 +1,73 @@
 package io.github.gianpamx.hellobluetooth.discovery
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.Snackbar.LENGTH_INDEFINITE
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import io.github.gianpamx.hellobluetooth.R
 
+
 class DiscoveryActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "DiscoveryActivity"
+        val REQUEST_ENABLE_BT = 1
+    }
+
+    lateinit var statusSnackbar: Snackbar
+
+    lateinit var discoveryViewModel: DiscoveryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.discovery_activity)
+
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        discoveryViewModel = ViewModelProviders.of(this, DiscoveryViewModel.Factory(bluetoothAdapter)).get(DiscoveryViewModel::class.java)
+
+        discoveryViewModel.error.observe(this, Observer {
+            it?.let {
+                Snackbar.make(findViewById(android.R.id.content), it, LENGTH_INDEFINITE).show()
+            }
+        })
+
+        statusSnackbar = Snackbar.make(findViewById(android.R.id.content), "", LENGTH_INDEFINITE)
+        discoveryViewModel.status.observe(this, Observer {
+            it?.let {
+                if (it == DiscoveryViewModel.State.STATE_OFF) {
+                    statusSnackbar.setText("Bluetooth OFF")
+                    statusSnackbar.setAction("Turn ON", View.OnClickListener {
+                        startActivityForResult(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT)
+                    })
+                    statusSnackbar.show()
+                } else if (it == DiscoveryViewModel.State.STATE_ON) {
+                    statusSnackbar.dismiss()
+                }
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            discoveryViewModel.checkStatus()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        registerReceiver(discoveryViewModel.stateChangeBroadcastReceiver, IntentFilter(ACTION_STATE_CHANGED))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(discoveryViewModel.stateChangeBroadcastReceiver)
     }
 }
---
+++ app/src/main/java/io/github/gianpamx/hellobluetooth/discovery/DiscoveryViewModel.kt
@@ -0,0 +1,60 @@
+package io.github.gianpamx.hellobluetooth.discovery
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DiscoveryViewModel : ViewModel {
+    val error = MutableLiveData<String>()
+    val status = MutableLiveData<State>()
+
+    private val bluetoothAdapter: BluetoothAdapter?
+
+    constructor(bluetoothAdapter: BluetoothAdapter?) {
+        this.bluetoothAdapter = bluetoothAdapter
+
+        if (this.bluetoothAdapter == null) {
+            error.value = "Devices do not support bluetooth"
+            return
+        }
+
+        checkStatus()
+    }
+
+    fun checkStatus() {
+        bluetoothAdapter?.let {
+            if (!it.isEnabled) {
+                status.postValue(State.STATE_OFF)
+            }
+        }
+    }
+
+    val stateChangeBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                if (it.extras.getInt(BluetoothAdapter.EXTRA_STATE) == BluetoothAdapter.STATE_OFF) {
+                    status.postValue(DiscoveryViewModel.State.STATE_OFF)
+                } else if (it.extras.getInt(BluetoothAdapter.EXTRA_STATE) == BluetoothAdapter.STATE_ON) {
+                    status.postValue(DiscoveryViewModel.State.STATE_ON)
+                }
+            }
+        }
+    }
+
+    class Factory(val bluetoothAdapter: BluetoothAdapter?) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return DiscoveryViewModel(bluetoothAdapter) as T
+        }
+    }
+
+    enum class State {
+        STATE_TURNING_ON,
+        STATE_ON,
+        STATE_TURNING_OFF,
+        STATE_OFF
+    }
+}
