46a2b0756a9143056e3ef092a986710470711b26
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-01-26 13:03:14 -0600

    first commit

---
+++ app/src/main/java/io/github/gianpamx/hellobluetooth/discovery/DiscoveryActivity.kt
@@ -0,0 +1,13 @@
+package io.github.gianpamx.hellobluetooth.discovery
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.gianpamx.hellobluetooth.R
+
+class DiscoveryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.discovery_activity)
+    }
+}
